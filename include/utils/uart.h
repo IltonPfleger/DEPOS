0c0aba68f4d1b690fc9a8c27029066e140a527ad
Ilton Pfleger Júnior <pfleger@lisha.ufsc.br>
2025-06-08 23:49:12 -0300

    fix uart

--- include/utils/uart.h
+++ include/utils/uart.h
@@ -1,41 +1,47 @@
-// ---- * UART 16550 * -----
 #ifndef UART_H
 #define UART_H
 
-#define UART_CLOCK      24000000UL    // 24 MHz, as unsigned long
+#define UART_CLOCK      24000000UL
 #define UART_BAUDRATE   115200U
-#define UART_BASE       (volatile char *)0x10000000
-#define UART_RBR        (UART_BASE + 0)  // Receiver Buffer Register (read only)
-#define UART_THR        (UART_BASE + 0)  // Transmitter Holding Register (write only)
-#define UART_IER        (UART_BASE + 1)  // Interrupt Enable Register
-#define UART_IIR        (UART_BASE + 2)  // Interrupt Identification Register (read only)
-#define UART_FCR        (UART_BASE + 2)  // FIFO Control Register (write only)
-#define UART_LCR        (UART_BASE + 3)  // Line Control Register
-#define UART_MCR        (UART_BASE + 4)  // Modem Control Register
-#define UART_LSR        (UART_BASE + 5)  // Line Status Register
-#define UART_MSR        (UART_BASE + 6)  // Modem Status Register
-#define UART_SCR        (UART_BASE + 7)  // Scratch Register
-#define UART_LSB        (UART_BASE + 0)  // Divisor Latch Low (when DLAB=1)
-#define UART_MSB        (UART_BASE + 1)  // Divisor Latch High (when DLAB=1)
+#define UART0           ((volatile uint8_t *)0x10000000)
+#define UART_RBR        (UART0 + 0)
+#define UART_THR        (UART0 + 0)
+#define UART_IER        (UART0 + 1)
+#define UART_IIR        (UART0 + 2)
+#define UART_FCR        (UART0 + 2)
+#define UART_LCR        (UART0 + 3)
+#define UART_MCR        (UART0 + 4)
+#define UART_LSR        (UART0 + 5)
+#define UART_MSR        (UART0 + 6)
+#define UART_SCR        (UART0 + 7)
+#define UART_LSB        (UART0 + 0)
+#define UART_MSB        (UART0 + 1)
 
-static void uart_init()
-{
-    // DISABLE INTERRUPTIONS
-    __asm__ volatile("sb %0, 0(%1)" ::"r"(0x00), "r"(UART_IER));
+#define LSR_TX_FIFO_EMPTY (1 << 5)
 
-    // SETUP CLOCK DIVISOR
-    const short DIVISOR = (UART_CLOCK + UART_BAUDRATE - 1) / (UART_BAUDRATE * 16);
-    __asm__ volatile("sb %0, 0(%1)" ::"r"(0x80), "r"(UART_LCR));
-    __asm__ volatile("sb %0, 0(%1)" ::"r"(DIVISOR & 0xFF), "r"(UART_LSB));
-    __asm__ volatile("sb %0, 0(%1)" ::"r"((DIVISOR >> 8) & 0xFF), "r"(UART_MSB));
+static void uart_init(void) {
+    // disable interrupts.
+    *UART_IER = 0x00;
 
-    // DISABLE CLOCK DIVISOR ACESS AND SETUP LCR
-    __asm__ volatile("sb %0, 0(%1)" ::"r"(0x03), "r"(UART_LCR));
+    // enable divisor latch acess
+    *UART_LCR         = (1 << 7);
+    const int divisor = (UART_CLOCK + UART_BAUDRATE * 8) / (UART_BAUDRATE * 16);
+    *UART_LSB         = divisor & 0xFF;
+    *UART_MSB         = (divisor >> 8) & 0xFF;
+
+    // 8 bits, no parity, 1 stop
+    *UART_LCR = 0x03;
+
+    // FIFO + reset TX/RX
+    *UART_FCR = 0x07;
+
+    // enable interrupts.
+    *UART_IER = 0x03;
 }
 
-static void uart_put(char c)
-{
-    *UART_BASE = c;
+static void uart_put(char c) {
+    while (!(*UART_LSR & LSR_TX_FIFO_EMPTY));
+    *UART_THR = c;
 }
 
 #endif
