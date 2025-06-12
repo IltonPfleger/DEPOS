ae9b0e288f76bf548fdc510e2687323530ddf8a4
Ilton Pfleger Júnior <pfleger@lisha.ufsc.br>
2025-06-12 17:43:37 -0300

    fix hearts

--- include/kernel.h
+++ include/kernel.h
@@ -16,9 +16,9 @@ void kmain() {
 }
 
 __attribute__((naked)) void ktrap() {
-    cpu_scontext();
-    kprint("EXCEPTION\n");
-    cpu_lcontext();
+    //cpu_scontext();
+    kprint("Trap!\n");
+    ///wcpu_lcontext();
     /* INTERRUPT OR EXCEPTION */
     //__asm__ volatile("csrr t0, mcause\nsrli t0, t0, %0" ::"i"(XLEN - 1));
     //__asm__ goto("beq t0, zero, %l[EXCEPTION]\nj %l[INTERRUPT]" ::
--- src/main.c
+++ src/main.c
@@ -6,12 +6,21 @@ __attribute__((naked, section(".boot"))) int bootloader()
     __asm__ volatile("csrci mstatus, 0x8");
 
     // stack.
-    __asm__ volatile("li sp, 0x80200000");
+	__asm__ volatile(
+        "la t0, cpus\n"
+        "csrr t1, mhartid\n"
+		"li t2, %0\n"
+        "mul t1, t1, t2\n"
+        "add sp, t0, t1\n"
+        :
+        : "i"(sizeof(cpus[0]))
+        : "t0", "t1", "t2");
 
     // trap handler.
     __asm__ volatile("la t0, ktrap\ncsrw mtvec, t0" ::: "t0");
 
-    kmain();
+	// start.
+    __asm__ volatile("j kmain");
 
     return 1;
 }
