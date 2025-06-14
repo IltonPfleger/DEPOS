e64cb4d5a6bfa0317ffa5c4974916019cede3bec
Ilton Pfleger Júnior <pfleger@lisha.ufsc.br>
2025-06-13 22:24:57 -0300

    update

---
+++ src/bootloader.cpp
@@ -0,0 +1,25 @@
+#include <definitions.hpp>
+
+extern void kmain();
+extern void ktrap();
+
+__attribute__((naked, section(".boot"))) void bootloader() {
+    // disable interrupts.
+    __asm__ volatile("csrci mstatus, 0x8");
+
+    // stack.
+    __asm__ volatile(
+        "la t0, _ZN3CPU5stackE\n"
+        "csrr t1, mhartid\n"
+        "li t2, %0\n"
+        "mul t1, t1, t2\n"
+        "add sp, t0, t1\n"
+        :
+        : "i"(Machine::Memory::PAGE_SIZE));
+
+    // trap handler.
+    __asm__ volatile("csrw mtvec, %0" ::"r"(ktrap));
+
+    // start.
+    __asm__ volatile("jr %0" ::"r"(kmain));
+}
--- src/kernel.cpp
+++ src/kernel.cpp
@@ -1,22 +1,15 @@
-#ifndef KERNEL_HPP
-#define KERNEL_HPP
-#include <utils/welcome.h>
-
-#include <CPU.hpp>
-#include <IO/IO.hpp>
-#include <IO/UART.hpp>
-#include <memory.hpp>
-
-using UART0 = UART<0x10000000, 32000000, 115200>;
-extern "C" char __KERNEL_END__[];
+#include <cpu.hpp>
+#include <io/io.hpp>
+#include <io/uart.hpp>
 
 void kmain() {
     if (CPU::id() == 0) {
         CPU::begin_atomic();
-        IO<UART0>::init();
-        Memory::init(reinterpret_cast<uintptr_t>(__KERNEL_END__));
-        Memory::alloc(13);
-        IO<UART0>::print(WELCOME);
+        IO<UART>::init();
+        IO<UART>::out("\nQ U A R K | μKernel\n");
+        // IO<UART0>::init();
+        // Memory::init(reinterpret_cast<uintptr_t>(__KERNEL_END__));
+        // Memory::alloc(13);
         CPU::end_atomic();
     } else {
         CPU::idle();
@@ -25,8 +18,13 @@ void kmain() {
 }
 
 __attribute__((naked)) void ktrap() {
-    IO<UART0>::print("Trap!\n");
-    while (1);
+    CPU::save();
+    IO<UART>::out("\nIt's a Trap!");
+    __asm__ volatile("j .");
+    CPU::load();
+
+    // IO<UART0>::print("Trap!\n");
+    // while (1);
     /* INTERRUPT OR EXCEPTION */
     //__asm__ volatile("csrr t0, mcause\nsrli t0, t0, %0" ::"i"(XLEN - 1));
     //__asm__ goto("beq t0, zero, %l[EXCEPTION]\nj %l[INTERRUPT]" ::
@@ -41,4 +39,3 @@ __attribute__((naked)) void ktrap() {
     // EXIT:
     //     __asm__ volatile("j .");
 }
-#endif
