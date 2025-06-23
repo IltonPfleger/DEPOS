50614546dab8c60547c5b0afe9444b1a9fb99bc6
Ilton Pfleger Júnior <pfleger@lisha.ufsc.br>
2025-06-23 14:02:01 -0300

    BACKUP

---
+++ src/app.cpp
@@ -0,0 +1,37 @@
+#include <io/logger.hpp>
+#include <thread.hpp>
+
+int teste0(void *) {
+    int i = 0;
+    while (i < 10) {
+        Logger::log("Thread0 %d\n", i);
+        Thread::yield();
+        i++;
+    }
+    return 0;
+}
+
+int teste1(void *) {
+    int i = 0;
+    while (i < 10) {
+        Logger::log("Thread1 %d\n", i);
+        Thread::yield();
+        i++;
+    }
+    return 0;
+}
+
+int main(void *) {
+    Thread thread0;
+    Thread thread1;
+
+    Thread::create(&thread0, teste0, Thread::Priority::NORMAL);
+    Thread::create(&thread1, teste1, Thread::Priority::NORMAL);
+
+    Thread::join(&thread0);
+    Logger::log("APP: thread0\n");
+    Thread::join(&thread1);
+    Logger::log("APP: thread1\n");
+
+    return 0;
+}
