9104d5e00f680896feaef6fdd604956f96659f78
Ilton Pfleger Júnior <pfleger@lisha.ufsc.br>
2025-06-27 01:09:38 -0300

    sync

--- build/modules/Definitions.d
+++
@@ -1,5 +0,0 @@
-build/modules/Definitions.o gcm.cache/Definitions.gcm: \
- modules/Definitions.cpp
-Definitions.c++-module: gcm.cache/Definitions.gcm
-.PHONY: Definitions.c++-module
-gcm.cache/Definitions.gcm:| build/modules/Definitions.o
--- build/modules/IO/UART.d
+++
@@ -1,4 +0,0 @@
-build/modules/IO/UART.o gcm.cache/UART.gcm: modules/IO/UART.cpp
-UART.c++-module: gcm.cache/UART.gcm
-.PHONY: UART.c++-module
-gcm.cache/UART.gcm:| build/modules/IO/UART.o
