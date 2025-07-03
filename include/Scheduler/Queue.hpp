42706cfbe08e00e93b093b33696607fe7926dc7a
Ilton Pfleger Júnior <pfleger@lisha.ufsc.br>
2025-07-03 15:02:15 -0300

    add FIFO to scheduler

--- include/Scheduler/Queue.hpp
+++ include/Scheduler/Queue.hpp
@@ -3,16 +3,13 @@
 #include <Memory.hpp>
 
 template <typename T>
-struct Stack {
+struct LIFO {
     struct Node {
         T value;
         Node *next;
     };
 
-    void put(T value) {
-        Node *node = new (Memory::SYSTEM) Node{.value = value, .next = head};
-        head       = node;
-    }
+    void put(T value) { head = new (Memory::SYSTEM) Node{.value = value, .next = head}; }
 
     T get() {
         if (!head) return nullptr;
@@ -36,4 +33,51 @@ struct Stack {
     }
 
     Node *head{nullptr};
+};
+
+template <typename T>
+struct FIFO {
+    struct Node {
+        T value;
+        Node *next;
+    };
+
+    void put(T value) {
+        Node *node = new (Memory::SYSTEM) Node{.value = value, .next = nullptr};
+        if (!head) {
+            head = tail = node;
+            return;
+        }
+        tail->next = node;
+        tail       = node;
+    }
+
+    T get() {
+        if (!head) return nullptr;
+        Node *node = head;
+        head       = node->next;
+        T value    = node->value;
+        delete node;
+        return value;
+    }
+
+    void remove(T value) {
+		if(!head) return;
+        Node *current  = head;
+        Node *previous = nullptr;
+        while (current && current->value != value) {
+            previous = current;
+            current  = current->next;
+        }
+
+        if (current == head) {
+            head = current->next;
+        } else if (current == tail) {
+            tail = previous;
+        }
+        delete current;
+    }
+
+    Node *head{nullptr};
+    Node *tail{nullptr};
 };
