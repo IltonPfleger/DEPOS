ac989da0062d6b848f192adcb894b154888f619d
Ilton Pfleger Júnior <pfleger@lisha.ufsc.br>
2025-07-15 12:12:20 -0300

    fix

--- include/Thread.hpp
+++ include/Thread.hpp
@@ -26,8 +26,9 @@ struct Thread {
 
     static inline volatile Thread *running() { return reinterpret_cast<volatile Thread *>(CPU::id()); };
     static inline void running(Thread *t) { t->state = RUNNING, CPU::id(t); }
-    static inline void save() { running()->context = CPU::Context::get(); };
-    static inline void load() { CPU::Context::set(running()->context); };
+
+    __attribute__((always_inline)) static inline void save() { running()->context = CPU::Context::get(); };
+    __attribute__((always_inline)) static inline void load() { CPU::Context::set(running()->context); };
 
     static void join(Thread *);
     static void exit();
