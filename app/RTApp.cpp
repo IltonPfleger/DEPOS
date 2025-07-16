c230d781fecfeb35cbb40b479ed3fd6db10705f6
Ilton Pfleger Júnior <pfleger@lisha.ufsc.br>
2025-07-15 21:27:13 -0300

    rate monotonic?

--- app/RTApp.cpp
+++ app/RTApp.cpp
@@ -3,12 +3,12 @@
 #include <Semaphore.hpp>
 #include <Thread.hpp>
 
-static constexpr int N = 10;
+static constexpr int N = 2;
 RT_Thread* thread[N];
 
 int teste(void* id) {
     while (1) {
-        Logger::println("THREAD: %d\n", (long long)id);
+        Logger::println("%d\n", (long long)id);
         RT_Thread::wait_next();
     }
     return 0;
@@ -17,7 +17,9 @@ int teste(void* id) {
 int main() {
     Logger::println("Real Time Application: \n");
 
-    for (int i = 0; i < N; i++) thread[i] = new (Memory::APPLICATION) RT_Thread(teste, (void*)(long long)i, 100'000);
+    // for (int i = 0; i < N; i++) thread[i] = new (Memory::APPLICATION) RT_Thread(teste, (void*)(long long)i, 1'000);
+    thread[0] = new (Memory::APPLICATION) RT_Thread(teste, (void*)(long long)0, 1'000);
+    thread[1] = new (Memory::APPLICATION) RT_Thread(teste, (void*)(long long)1, 100'000);
 
     for (int i = 0; i < N; i++) {
         Thread::join(thread[i]);
