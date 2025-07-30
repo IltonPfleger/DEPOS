0229f78f2c2b52c6fe0a5b61cca4d2f208bd0012
Ilton Pfleger Júnior <pfleger@lisha.ufsc.br>
2025-07-30 17:42:34 -0300

    semaphore uses a global spin don't need atomic fdec/finc

--- include/Semaphore.hpp
+++ include/Semaphore.hpp
@@ -4,14 +4,15 @@
 #include <Thread.hpp>
 
 class Semaphore {
-    volatile int value = 1;
+    int value = 1;
     Thread::Queue waiting;
     Spin spin;
 
    public:
     void p() {
         Thread::lock();
-        if (CPU::Atomic::fdec(value) < 1)
+        value--;
+        if (value + 1 < 1)
             Thread::sleep(waiting);
         else
             Thread::unlock();
@@ -20,7 +21,8 @@ class Semaphore {
 
     void v() {
         Thread::lock();
-        if (CPU::Atomic::finc(value) < 0) Thread::wakeup(waiting);
+        value++;
+        if (value - 1 < 0) Thread::wakeup(waiting);
         Thread::unlock();
     }
 };
