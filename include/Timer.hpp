9ac7383430b5d74bc2b768673b8ffa50d59bbd5b
Ilton Pfleger Júnior <pfleger@lisha.ufsc.br>
2025-08-08 17:59:41 -0300

    move trap handler to CPU and use as vector

--- include/Timer.hpp
+++ include/Timer.hpp
@@ -5,44 +5,35 @@
 #include <Memory.hpp>
 #include <Traits.hpp>
 
-template <bool enabled = Traits::Timer::Enable>
-class _Timer {
-   public:
-    static void handler() {}
-    static void init() {}
-};
-
-template <>
-class _Timer<true> {
-    enum { SCHEDULER, ALARM, COUNT };
+namespace Timer {
     struct Channel {
         Tick _initial;
         Tick _current[Machine::CPUS];
     };
 
-    static inline Channel _channels[COUNT];
+    static inline Channel _scheduler;
 
     static void reset() {
+        static constexpr uintmax_t ticks = Machine::CLINT::CLOCK / Traits::Timer::Frequency;
         *reinterpret_cast<volatile uintmax_t *>(Machine::CLINT::MTIMECMP + (CPU::core() * 8)) =
-            *Machine::CLINT::MTIME + (Machine::CLINT::CLOCK / Traits::Timer::Frequency);
+            *Machine::CLINT::MTIME + ticks;
     }
 
-   public:
-    static void init() {
+    void init() {
         if constexpr (Traits::Scheduler<Thread>::Criterion::Timed) {
-            _channels[SCHEDULER]._initial = Traits::Timer::Frequency / Traits::Scheduler<Thread>::Frequency;
-            _channels[SCHEDULER]._current[CPU::core()] = _channels[SCHEDULER]._initial;
+            _scheduler._initial              = Traits::Timer::Frequency / Traits::Scheduler<Thread>::Frequency;
+            _scheduler._current[CPU::core()] = _scheduler._initial;
         }
 
-        if constexpr (Traits::Alarm::Enable) {
-            _channels[ALARM]._initial              = Traits::Timer::Frequency / Traits::Alarm::Frequency;
-            _channels[ALARM]._current[CPU::core()] = _channels[ALARM]._initial;
-        }
+        // if constexpr (Traits::Alarm::Enable) {
+        //     _channels[ALARM]._initial              = Traits::Timer::Frequency / Traits::Alarm::Frequency;
+        //     _channels[ALARM]._current[CPU::core()] = _channels[ALARM]._initial;
+        // }
 
         CPU::Interrupt::Timer::enable();
     }
 
-    static void handler() {
+    void handler() {
         reset();
         //  if constexpr (Traits::Alarm::Enable) {
         //      if (--_channels[ALARM]._current[CPU::core()] == 0) {
@@ -52,12 +43,10 @@ class _Timer<true> {
         //  }
 
         if constexpr (Traits::Scheduler<Thread>::Criterion::Timed) {
-            if (--_channels[SCHEDULER]._current[CPU::core()] == 0) {
-                _channels[SCHEDULER]._current[CPU::core()] = _channels[SCHEDULER]._initial;
+            if (--_scheduler._current[CPU::core()] == 0) {
+                _scheduler._current[CPU::core()] = _scheduler._initial;
                 Thread::reschedule();
             }
         }
     }
 };
-
-using Timer = _Timer<>;
