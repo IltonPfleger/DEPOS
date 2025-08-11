c8838ec02ece3b3d250312e5b1fbe52d32bd5a80
Ilton Pfleger Júnior <pfleger@lisha.ufsc.br>
2025-08-11 18:12:07 -0300

    organize save/load

--- include/CPU.hpp
+++ include/CPU.hpp
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <Machine.hpp>
+#define OFFSET(type, member) ((unsigned long)&(((type *)0)->member))
 
 namespace Timer {
     void handler();
@@ -51,52 +52,56 @@ struct CPU {
         __attribute__((always_inline)) static inline void save() {
             __asm__ volatile(
                 "addi sp, sp, %0\n"
-                "sd ra, 0(sp)\n"
-                "sd tp, 8(sp)\n"
-                "sd s0, 136(sp)\n"
-                "sd s1, 144(sp)\n"
-                "sd s2, 152(sp)\n"
-                "sd s3, 160(sp)\n"
-                "sd s4, 168(sp)\n"
-                "sd s5, 176(sp)\n"
-                "sd s6, 184(sp)\n"
-                "sd s7, 192(sp)\n"
-                "sd s8, 200(sp)\n"
-                "sd s9, 208(sp)\n"
-                "sd s10, 216(sp)\n"
-                "sd s11, 224(sp)\n" ::"i"(-sizeof(Context))
+                "sd ra, %c[ra](sp)\n"
+                "sd tp, %c[tp](sp)\n"
+                "sd s0, %c[s0](sp)\n"
+                "sd s1, %c[s1](sp)\n"
+                "sd s2, %c[s2](sp)\n"
+                "sd s3, %c[s3](sp)\n"
+                "sd s4, %c[s4](sp)\n"
+                "sd s5, %c[s5](sp)\n"
+                "sd s6, %c[s6](sp)\n"
+                "sd s7, %c[s7](sp)\n"
+                "sd s8, %c[s8](sp)\n"
+                "sd s9, %c[s9](sp)\n"
+                "sd s10, %c[s10](sp)\n"
+                "sd s11, %c[s11](sp)\n"
+                :
+                : "i"(-sizeof(Context)), [ra] "i"(OFFSET(Context, ra)), [tp] "i"(OFFSET(Context, tp)),
+                  [s0] "i"(OFFSET(Context, s0)), [s1] "i"(OFFSET(Context, s1)), [s2] "i"(OFFSET(Context, s2)),
+                  [s3] "i"(OFFSET(Context, s3)), [s4] "i"(OFFSET(Context, s4)), [s5] "i"(OFFSET(Context, s5)),
+                  [s6] "i"(OFFSET(Context, s6)), [s7] "i"(OFFSET(Context, s7)), [s8] "i"(OFFSET(Context, s8)),
+                  [s9] "i"(OFFSET(Context, s9)), [s10] "i"(OFFSET(Context, s10)), [s11] "i"(OFFSET(Context, s11))
                 : "memory");
         }
 
         __attribute__((naked)) static void load(Context *c) {
-            asm volatile(
-                "mv ra, %0\n"
-                "mv tp, %1\n"
-                "mv sp, %2\n"
+            (void)c;
+            __asm__ volatile(
+                "mv sp, a0\n"
+                "ld ra, %c[ra](sp)\n"
+                "ld tp, %c[tp](sp)\n"
+                "ld s0, %c[s0](sp)\n"
+                "ld s1, %c[s1](sp)\n"
+                "ld s2, %c[s2](sp)\n"
+                "ld s3, %c[s3](sp)\n"
+                "ld s4, %c[s4](sp)\n"
+                "ld s5, %c[s5](sp)\n"
+                "ld s6, %c[s6](sp)\n"
+                "ld s7, %c[s7](sp)\n"
+                "ld s8, %c[s8](sp)\n"
+                "ld s9, %c[s9](sp)\n"
+                "ld s10, %c[s10](sp)\n"
+                "ld s11, %c[s11](sp)\n"
+                "addi sp, sp, %0\n"
+                "ret\n"
                 :
-                : "r"(c->ra), "r"(c->tp), "r"(c)
+                : "i"(sizeof(Context)), [ra] "i"(OFFSET(Context, ra)), [tp] "i"(OFFSET(Context, tp)),
+                  [s0] "i"(OFFSET(Context, s0)), [s1] "i"(OFFSET(Context, s1)), [s2] "i"(OFFSET(Context, s2)),
+                  [s3] "i"(OFFSET(Context, s3)), [s4] "i"(OFFSET(Context, s4)), [s5] "i"(OFFSET(Context, s5)),
+                  [s6] "i"(OFFSET(Context, s6)), [s7] "i"(OFFSET(Context, s7)), [s8] "i"(OFFSET(Context, s8)),
+                  [s9] "i"(OFFSET(Context, s9)), [s10] "i"(OFFSET(Context, s10)), [s11] "i"(OFFSET(Context, s11))
                 : "memory");
-
-            asm volatile(
-                "mv s0, %0\n"
-                "mv s1, %1\n"
-                "mv s2, %2\n"
-                "mv s3, %3\n"
-                "mv s4, %4\n"
-                "mv s5, %5\n"
-                "mv s6, %6\n"
-                "mv s7, %7\n"
-                "mv s8, %8\n"
-                "mv s9, %9\n"
-                "mv s10, %10\n"
-                "mv s11, %11\n"
-                :
-                : "r"(c->s0), "r"(c->s1), "r"(c->s2), "r"(c->s3), "r"(c->s4), "r"(c->s5), "r"(c->s6), "r"(c->s7),
-                  "r"(c->s8), "r"(c->s9), "r"(c->s10), "r"(c->s11)
-                : "memory");
-            asm volatile("mv a0, %0" ::"r"(c->a0));
-            asm volatile("addi sp, sp, %0" ::"i"(sizeof(Context)));
-            CPU::ret();
         }
 
         __attribute__((always_inline)) static inline void push() {
