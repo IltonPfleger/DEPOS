df01cfa32782b44934acc056235ab2d47f3d4d35
Ilton Pfleger Júnior <pfleger@lisha.ufsc.br>
2025-08-21 22:10:26 -0300

    working machine mode

--- src/Kernel.cpp
+++ src/Kernel.cpp
@@ -6,7 +6,6 @@
 #include <Thread.hpp>
 #include <Timer.hpp>
 
-static char STACK[Machine::CPUS][Traits::Memory::Page::SIZE];
 static volatile bool boot = true;
 
 namespace Kernel {
@@ -28,29 +27,27 @@ namespace Kernel {
         for (;;);
     }
 
-    void exception() {
-        uintptr_t mcause, mepc, mtval;
-        __asm__ volatile("csrr %0, mcause" : "=r"(mcause));
-        __asm__ volatile("csrr %0, mepc" : "=r"(mepc));
-        __asm__ volatile("csrr %0, mtval" : "=r"(mtval));
-        ERROR(true,
-              "Ohh it's a Trap!\n"
-              "mcause: %p\n"
-              "mepc: %p\n"
-              "mtval: %p\n",
-              mcause, mepc, mtval);
+    __attribute__((naked, aligned(4))) void ktrap() {
+        CPU::Context::push();
+        CPU::Trap::handler();
+        CPU::Context::pop();
     }
-}
 
-__attribute__((naked, aligned(4))) void ktrap() {
-    CPU::Context::push();
-    CPU::Trap::handler();
-    CPU::Context::pop();
+    void exception() {
+        // uintptr_t mcause, mepc, mtval;
+        //__asm__ volatile("csrr %0, mcause" : "=r"(mcause));
+        //__asm__ volatile("csrr %0, mepc" : "=r"(mepc));
+        //__asm__ volatile("csrr %0, mtval" : "=r"(mtval));
+        // ERROR(true,
+        //       "Ohh it's a Trap!\n"
+        //       "mcause: %p\n"
+        //       "mepc: %p\n"
+        //       "mtval: %p\n",
+        //       mcause, mepc, mtval);
+    }
 }
 
 __attribute__((naked, section(".boot"))) void kboot() {
     CPU::init();
-    CPU::Trap::set(ktrap);
-    CPU::stack(STACK[CPU::core()] + Traits::Memory::Page::SIZE);
     Kernel::init();
 }
