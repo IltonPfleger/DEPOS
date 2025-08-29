1f157eb4d98697a02ba37beff8c29f49fd7f01fa
Ilton Pfleger Júnior <pfleger@lisha.ufsc.br>
2025-08-28 23:36:21 -0300

    remove trash

--- include/cpus/riscv/cpu.hpp
+++ include/cpus/riscv/cpu.hpp
@@ -17,17 +17,15 @@ class RISCV {
     typedef uintmax_t Register;
     struct Machine {
         enum : Register {
-            PMPADDR0 = 0x3B0,
-            PMPCFG0  = 0x3A0,
-            MHARTID  = 0xF14,  // Core Number/ID
-            MEDELEG  = 0x302,  // Machine Exception Delegation
-            MIDELEG  = 0x303,  // Machine Interrupt Delegation
-            // PP       = 3 << 11,     // Previous Previlege
-            ME2ME = 3ULL << 11,  // Machine to Machine
-            // ME2USER       = ME2ME,       // Machine to User
+            PMPADDR0      = 0x3B0,
+            PMPCFG0       = 0x3A0,
+            MHARTID       = 0xF14,       // Core Number/ID
+            MEDELEG       = 0x302,       // Machine Exception Delegation
+            MIDELEG       = 0x303,       // Machine Interrupt Delegation
+            ME2ME         = 3ULL << 11,  // Machine to Machine
             ME2SUPERVISOR = 1ULL << 11,  // Machine to Supervisor
-            IRQE          = 1ULL << 3,   // Interrupt Enable
             TI            = 1ULL << 7,   // Timer Interrupt Enable
+            IRQE          = 1ULL << 3,   // Interrupt Enable
             PIRQE         = 1ULL << 7,   // Previous Interrupt Enable
         };
         static constexpr const int STATUS = 0x300;
@@ -42,11 +40,9 @@ class RISCV {
 
     struct Supervisor {
         enum : Register {
-            // PP    = 1ULL << 8,  // Previous Previlege
             ME2ME = 1ULL << 8,  // Supervisor to Supervisor
-            // ME2USER = ME2ME,      // Supervisor to User
-            IRQE  = 1ULL << 1,  // Interrupt Enable
             TI    = 1ULL << 5,  // Timer Interrupt Enable
+            IRQE  = 1ULL << 1,  // Interrupt Enable
             PIRQE = 1ULL << 5,  // Previous Interrupt Enable
         };
         static constexpr const int STATUS = 0x100;
@@ -98,7 +94,6 @@ class RISCV {
     __attribute__((naked)) static void init() {
         asm volatile("csrr tp, mhartid");
         sp(stack[core()] + Traits::Memory::Page::SIZE);
-        // RISCV::csrc<MSTATUS>(RISCV::MACHINE_IRQ);
 
         if constexpr (Meta::StringCompare(Traits::Machine::NAME, "sifive_u") && Meta::SAME<Mode, Supervisor>::Result) {
             if (core() == 0) {
