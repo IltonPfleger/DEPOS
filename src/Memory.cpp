605fb55947e0e9d9f315cccbd71018893edad2e2
Ilton Pfleger Júnior <pfleger@lisha.ufsc.br>
2025-10-29 20:57:15 -0300

    fix memory

--- src/Memory.cpp
+++ src/Memory.cpp
@@ -10,15 +10,15 @@ void Memory::init() {
     TRACE(__PRETTY_FUNCTION__, "{");
 
     constexpr unsigned int PAGE_SIZE = Traits::Memory::Page::SIZE;
+    char* RAM_BASE = reinterpret_cast<char*>(Traits::Memory::RAM_BASE);
     char* RAM_END                    = reinterpret_cast<char*>(Traits::Memory::RAM_END);
     char* KERNEL_START               = const_cast<char*>(__KERNEL_START__);
     char* KERNEL_END                 = const_cast<char*>(__KERNEL_END__);
     const unsigned int KERNEL_SIZE   = KERNEL_END - KERNEL_START;
     const unsigned int HEAP_SIZE     = Traits::Memory::SIZE - KERNEL_SIZE;
-    char* RAM_BASE                   = reinterpret_cast<char*>(Traits::Memory::RAM_BASE) + KERNEL_SIZE;
 
-    char* c = RAM_BASE;
-    for (; c + PAGE_SIZE < RAM_END; c += PAGE_SIZE) {
+    char* c;
+    for (c = RAM_BASE; c + PAGE_SIZE < RAM_END; c += PAGE_SIZE) {
         if (c >= KERNEL_START && c < KERNEL_END) continue;
         pages.insert(reinterpret_cast<Page*>(c));
     }
@@ -26,8 +26,6 @@ void Memory::init() {
     TRACE("KernelStart=", static_cast<void*>(KERNEL_START), ", ");
     TRACE("KernelEnd=", static_cast<void*>(KERNEL_END), ", ");
     TRACE("KernelSize=", KERNEL_SIZE, ", ");
-    TRACE("HeapStart=", static_cast<void*>(RAM_BASE), ", ");
-    TRACE("HeapEnd=", static_cast<void*>(c), ", ");
     TRACE("HeapSize=", HEAP_SIZE);
     TRACE("}\n");
 }
