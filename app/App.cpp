78eff2813671bf3a2188ed5139feed2f976c0c0f
Ilton Pfleger Júnior <pfleger@lisha.ufsc.br>
2025-11-01 21:10:24 -0300

    sync

--- app/App.cpp
+++ app/App.cpp
@@ -5,39 +5,39 @@
 #include <Semaphore.hpp>
 #include <Thread.hpp>
 
-static constexpr int N          = 5;
-static constexpr int ITERATIONS = 5;
-
-static Thread *threads[N];
-static Semaphore mutex;
-
-int thread_function(void *arg) {
-    int id = (int)(long long)arg;
-    int i  = ITERATIONS;
-    while (i--) {
-        mutex.p();
-        Console::out << id << " ";
-        // Console::println("THREAD: %d | Core: %d\n", id, Machine::CPU::core());
-        mutex.v();
-        Thread::yield();
-        //    Alarm::usleep(10000);
-    }
-    return 0;
-}
+// static constexpr int N          = 5;
+// static constexpr int ITERATIONS = 5;
+//
+// static Thread *threads[N];
+// static Semaphore mutex;
+//
+// int thread_function(void *arg) {
+//     int id = (int)(long long)arg;
+//     int i  = ITERATIONS;
+//     while (i--) {
+//         mutex.p();
+//         Console::out << id << " ";
+//         // Console::println("THREAD: %d | Core: %d\n", id, Machine::CPU::core());
+//         mutex.v();
+//         Thread::yield();
+//         //    Alarm::usleep(10000);
+//     }
+//     return 0;
+// }
 
 int main(void *) {
-    Console::out << "Application: \n";
+    // Console::out << "Application: \n";
 
-    mutex.p();
-    for (int i = 0; i < N; i++) {
-        threads[i] = new Thread(thread_function, (void *)(long long)i, Thread::Criterion::NORMAL);
-    }
-    mutex.v();
+    // mutex.p();
+    // for (int i = 0; i < N; i++) {
+    //     threads[i] = new Thread(thread_function, (void *)(long long)i, Thread::Criterion::NORMAL);
+    // }
+    // mutex.v();
 
-    for (int i = 0; i < N; i++) Thread::join(*threads[i]);
+    // for (int i = 0; i < N; i++) Thread::join(*threads[i]);
 
-    // for (int i = 0; i < N; i++) delete threads[i];
+    //// for (int i = 0; i < N; i++) delete threads[i];
 
-    Console::out << "\nApplication Done!\n";
+    // Console::out << "\nApplication Done!\n";
     return 0;
 }
