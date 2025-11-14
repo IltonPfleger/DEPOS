4144df3e4ac41c9b91466ea1033f6447746dc121
Ilton Pfleger Júnior <pfleger@lisha.ufsc.br>
2025-11-14 07:46:00 -0300

    sync

--- include/Application.hpp
+++ include/Application.hpp
@@ -7,23 +7,34 @@
 #include <memory/MemoryMap.hpp>
 
 class Application {
-  public:
+   public:
     static void init() {
-        // if constexpr (Traits<System>::MULTITASK) {
-        //    uintptr_t CodeStart = reinterpret_cast<uintptr_t>(__mm.app.code.start);
-        //    uintptr_t CodeEnd   = reinterpret_cast<uintptr_t>(__mm.app.code.end);
-        //    TraceIn(CodeStart, CodeEnd);
+        auto main = reinterpret_cast<int (*)(void *)>(__mm.main);
+        if constexpr (Traits<System>::MULTITASK) {
+            uintptr_t TextStart = __mm.text.start;
+            uintptr_t TextEnd   = __mm.text.end;
+            uintptr_t TextSize  = TextEnd - TextStart;
 
-        //    Task *task   = new (Heap::SYSTEM) Task();
-        //    Segment code = Segment(__mm.app.code.start, CodeEnd - CodeStart);
-        //    task->attach(code);
+            uintptr_t DataStart = __mm.data.start;
+            uintptr_t DataEnd   = __mm.data.end;
+            uintptr_t DataSize  = DataEnd - DataStart;
 
-        //    new (Heap::SYSTEM)
-        //        Thread(reinterpret_cast<int (*)(void *)>(__mm.app.entry), 0, Thread::Criterion::NORMAL, task);
-        //    TraceOut();
+            uintptr_t BssStart = __mm.bss.start;
+            uintptr_t BssEnd   = __mm.bss.end;
+            uintptr_t BssSize  = BssEnd - BssStart;
 
-        //} else {
-        new (Heap::SYSTEM) Thread(reinterpret_cast<int (*)(void *)>(__mm.main), 0, Thread::Criterion::NORMAL);
-        //}
+            TraceIn(TextStart, TextSize, DataStart, DataSize, BssStart, BssSize);
+
+            Task *task = new (Heap::SYSTEM) Task();
+            //    Segment text = Segment(__mm.app.text.start, TextEnd - TextStart);
+            //    task->attach(text);
+
+            //    new (Heap::SYSTEM)
+            //        Thread(reinterpret_cast<int (*)(void *)>(__mm.app.entry), 0, Thread::Criterion::NORMAL, task);
+            //    TraceOut();
+            new (Heap::SYSTEM) Thread(main, 0, Thread::Criterion::NORMAL, task);
+        } else {
+            new (Heap::SYSTEM) Thread(main, 0, Thread::Criterion::NORMAL);
+        }
     };
 };
