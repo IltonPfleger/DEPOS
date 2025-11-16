51ce87f9dbb49c1f157fae4dff985bdaf60c9448
Ilton Pfleger Júnior <pfleger@lisha.ufsc.br>
2025-11-16 19:24:03 -0300

    sync

--- include/Timer.hpp
+++ include/Timer.hpp
@@ -1,7 +1,5 @@
 #pragma once
-// #include <Alarm.hpp>
 #include <Traits.hpp>
-// #include <Types.hpp>
 
 class Timer {
     struct Channel {
@@ -13,20 +11,5 @@ class Timer {
 
   public:
     static void handler(unsigned long);
-    static void init() {
-        if constexpr (Traits<Scheduler<Thread>>::Preemptive) {
-            _scheduler._default =
-                Traits<Timer>::Frequency / Traits<Scheduler<Thread>>::Frequency;
-            for (auto &e : _scheduler._current)
-                e = _scheduler._default;
-            //_scheduler._current[Machine::CPU::core()] = _scheduler._default;
-        }
-
-        // if constexpr (Traits<Alarm>::Enable) {
-        //     _channels[ALARM]._default              = Traits<Timer>::Frequency
-        //     / Traits<Alarm>::Frequency;
-        //     _channels[ALARM]._current[CPU::core()] =
-        //     _channels[ALARM]._default;
-        // }
-    }
+    static void init();
 };
