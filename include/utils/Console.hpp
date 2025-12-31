6acae48da05dee4aacc17a5c6b510bbdc6574aa3
Ilton Pfleger Júnior <pfleger@lisha.ufsc.br>
2025-12-31 02:13:30 -0300

    sync

--- include/utils/Console.hpp
+++ include/utils/Console.hpp
@@ -1,8 +1,7 @@
 #pragma once
+
 #include <Meta.hpp>
 #include <Variadic.hpp>
-
-static inline constexpr char HEX[] = "0123456789ABCDEF";
 
 struct Console {
     static void init();
@@ -12,63 +11,78 @@ struct Console {
         using Manipulator = Stream &(*)(Stream &);
 
       public:
-        inline Stream &operator<<(Manipulator m) { return m(*this); }
+        Stream &operator<<(Manipulator m) { return m(*this); }
 
-        inline Stream &operator<<(char c) {
+        Stream &operator<<(char c) {
             Console::put(c);
             return *this;
         }
 
-        inline Stream &operator<<(const char *str) {
+        Stream &operator<<(const char *str) {
+            if (!str)
+                return *this;
+
             while (*str)
                 Console::put(*str++);
             return *this;
         }
 
-        template <Meta::INTEGRAL T> inline Stream &operator<<(T value) {
+        Stream &operator<<(unsigned long value) {
             char buffer[32];
-            int position = 0;
+            int pos = 0;
 
-            if constexpr (Meta::SIGNED<T>::Result) {
+            do {
+                buffer[pos++] = char('0' + (value % 10));
+                value /= 10;
+            } while (value);
+
+            while (pos--)
+                Console::put(buffer[pos]);
+
+            return *this;
+        }
+
+        template <Meta::Integral T> Stream &operator<<(T value) {
+            if constexpr (Meta::Signed<T>::Result) {
                 if (value < 0) {
                     Console::put('-');
                     value = -value;
                 }
             }
-
-            do {
-                buffer[position++] = '0' + (value % 10);
-                value /= 10;
-            } while (value > 0);
-
-            while (position--)
-                Console::put(buffer[position]);
-            return *this;
+            return *this << static_cast<unsigned long>(value);
         }
 
-        inline Stream &operator<<(const void *pointer) {
-            auto addr = reinterpret_cast<unsigned long>(pointer);
-            Console::put('0');
-            Console::put('x');
-            for (int i = (sizeof(void *) * 2) - 1; i >= 0; i--) {
-                Console::put(HEX[(addr >> (i * 4)) & 0xF]);
+        Stream &operator<<(const void *ptr) {
+            auto hex = reinterpret_cast<unsigned long>(ptr);
+            put('0');
+            put('x');
+            bool started = false;
+
+            for (int i = (sizeof(hex) * 8) - 4; i >= 0; i -= 4) {
+                unsigned int current = (hex >> i) & 0xF;
+                if (current != 0 || started) {
+                    put(current < 10 ? current + '0' : current - 10 + 'a');
+                    started = true;
+                }
+            }
+            if (!started) {
+                put('0');
             }
             return *this;
         }
 
         static Stream &endl(Stream &s) {
-            put('\n');
+            Console::put('\n');
             return s;
         }
     };
-
-    template <typename... Args> static void cprintln(Args &&...args) {
+    template <typename... Args> static void print(Args &&...args) {
         bool first = true;
-        ((Console::out << (first ? (first = false, "") : ", ") << args), ...);
+        ((Console::out << (first ? (first = false, "") : ", ") << static_cast<Args &&>(args)), ...);
     }
 
     static void println(const char *, ...);
 
   public:
-    static inline Stream out;
+    static inline Stream out{};
 };
--- include/utils/Debug.hpp
+++ include/utils/Debug.hpp
@@ -16,7 +16,7 @@
 #define TraceIn(...)                                                           \
     if constexpr (Traits<Debug>::Trace) {                                      \
         Console::println("<%d> %s(", CPU::id(), __PRETTY_FUNCTION__);          \
-        __VA_OPT__(Console::cprintln(__VA_ARGS__));                            \
+        __VA_OPT__(Console::print(__VA_ARGS__));                            \
         Console::println("){\n");                                              \
     }
 
