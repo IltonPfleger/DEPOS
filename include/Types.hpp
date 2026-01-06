dc32dc573ea4582c9bc6105a4334ec65a0b5ac09
Ilton Pfleger Júnior <ilton.pfleger@grad.ufsc.br>
2026-01-05 23:15:46 -0300

    sync

--- include/Types.hpp
+++ include/Types.hpp
@@ -1,20 +1,20 @@
 #pragma once
 #include <Meta.hpp>
 
-typedef Meta::TypeSelector<sizeof(void *) == 8, long long, void>::Result int64_t;
+typedef Meta::TypeSelector<sizeof(long) == 8, long, void>::Result int64_t;
 typedef int int32_t;
 typedef short int16_t;
 typedef char int8_t;
 
-typedef Meta::TypeSelector<sizeof(void *) == 8, unsigned long long, void>::Result uint64_t;
+typedef Meta::TypeSelector<sizeof(unsigned long) == 8, unsigned long, void>::Result uint64_t;
 typedef unsigned int uint32_t;
 typedef unsigned short uint16_t;
 typedef unsigned char uint8_t;
 
-typedef Meta::TypeSelector<sizeof(void *) == 8, long long, int>::Result intmax_t;
-typedef Meta::TypeSelector<sizeof(void *) == 8, long long, int>::Result intptr_t;
-typedef Meta::TypeSelector<sizeof(void *) == 8, unsigned long long, unsigned>::Result uintptr_t;
-typedef Meta::TypeSelector<sizeof(void *) == 8, unsigned long long, unsigned>::Result uintmax_t;
+typedef Meta::TypeSelector<sizeof(void *) == 8, long, int>::Result intmax_t;
+typedef Meta::TypeSelector<sizeof(void *) == 8, unsigned long, unsigned int>::Result uintmax_t;
+typedef intmax_t intptr_t;
+typedef uintmax_t uintptr_t;
 
 typedef unsigned long size_t;
 typedef uintmax_t Microsecond;
