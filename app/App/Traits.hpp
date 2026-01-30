25ccf69af38065a343ae2b4d759791aede1163e2
Ilton Pfleger Júnior <ilton.pfleger@grad.ufsc.br>
2026-01-29 22:27:51 -0300

    sync

--- app/App/Traits.hpp
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#include <machine/Traits.hpp>
-
-class Application;
-
-template <> struct Traits<Application> {
-    static constexpr unsigned long Addr = Traits<MemoryMap>::RamStart + 0x00200000;
-};
