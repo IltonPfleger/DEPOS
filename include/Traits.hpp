cbe6c1510ca52ededb04e1766f6548970a2521bd
Ilton Pfleger Júnior <ilton.pfleger@grad.ufsc.br>
2026-02-01 16:05:44 -0300

    run linux in a memory container

--- include/Traits.hpp
+++ include/Traits.hpp
@@ -15,7 +15,7 @@ template <typename U> class Scheduler;
 template <typename T> struct Traits;
 
 template <> struct Traits<System> {
-    static constexpr bool Multitask = true;
+    static constexpr bool Multitask = false;
 };
 
 template <> struct Traits<Timer> {
