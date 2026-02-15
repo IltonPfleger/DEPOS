04402d9bf431a6b7c76adfd115413fc848b917f3
Ilton Pfleger Júnior <ilton.pfleger@grad.ufsc.br>
2026-02-15 17:44:27 -0300

    add ostream operator

---
+++ include/abstractions/IC.hpp
@@ -0,0 +1,7 @@
+#pragma once
+
+#include <Macros.hpp>
+
+#include __IC_HEADER
+
+class IC : public __ARCH::IC {};
--- include/utils/Console.hpp
+++ include/utils/Console.hpp
@@ -9,6 +9,79 @@ class Console {
     static void put(char);
 
   public:
+    class Stream {
+        friend Console;
+        using Modifier = Stream &(*)(Stream &);
+
+      public:
+        Stream() : m_base(10), m_panic(0) {}
+
+        Stream &operator<<(Modifier modifier) { return modifier(*this); }
+
+        Stream &operator<<(char c) { return put(c), *this; }
+
+        Stream &operator<<(const char *s) {
+            while (s && *s)
+                *this << *s++;
+            return *this;
+        }
+
+        template <unsigned Length> Stream &operator<<(const char (&s)[Length]) { return *this << static_cast<const char *>(s); }
+
+        template <Meta::Pointer T> Stream &operator<<(T p) { return *this << reinterpret_cast<void *>(p); }
+
+        template <Meta::Integer T> Stream &operator<<(T x) {
+            if constexpr (Meta::Signed<T>::Result) {
+                if (m_base == 10 && x < 0) {
+                    *this << '-';
+                    x = static_cast<unsigned long>(-x);
+                }
+            }
+
+            if (x == 0) {
+                *this << '0';
+                return *this;
+            }
+
+            char buffer[64];
+            int i = 0;
+            const char *digits = "0123456789abcdef";
+
+            while (x > 0) {
+                buffer[i++] = digits[x % m_base];
+                x /= m_base;
+            }
+
+            if (m_base == 16) *this << "0x";
+
+            while (i > 0) {
+                *this << buffer[--i];
+            }
+
+            return *this;
+        }
+
+        Stream &operator<<(void *p) {
+            unsigned int old = m_base;
+            m_base = 16;
+            *this << reinterpret_cast<uintptr_t>(p);
+            m_base = old;
+            return *this;
+        }
+
+        bool panicked() {
+            unsigned char volatile guard;
+            if (m_panic) return true;
+            if (m_panic == (reinterpret_cast<unsigned long>(&guard) & ~(Traits<Memory>::StackSize - 1))) return true;
+            return false;
+        }
+
+      private:
+        unsigned int m_base;
+        unsigned long m_panic;
+    };
+
+  public:
     template <typename T> struct Hex {
         explicit Hex(T x) : m_x(x) {}
         operator T() const { return m_x; }
@@ -59,6 +132,9 @@ class Console {
         Console::print(", ");
         Console::print(static_cast<Second &&>(second), static_cast<Args &&>(args)...);
     }
+
+  public:
+    static inline Stream cout;
 
   private:
     static inline unsigned int s_panic = 0;
