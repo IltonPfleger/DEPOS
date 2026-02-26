120c55e402c62df8e565c0ed1f530ec286c3d497
Ilton Pfleger Júnior <ilton.pfleger@grad.ufsc.br>
2026-02-26 00:01:43 -0300

    sync

--- tools/Traits.cpp
+++ tools/Traits.cpp
@@ -5,13 +5,13 @@ using namespace std;
 using namespace DEPOS;
 
 int main() {
-    cout << "MACHINE_NAME := " << Traits<Machine>::NAME << endl;
-    cout << "ARCH := " << Traits<CPU>::Architecture << endl;
-    cout << "CPUS := " << Traits<CPU>::Count << endl;
-    cout << "MEMORY_SIZE := " << Traits<Memory>::Size << endl;
-    cout << "PAGE_SIZE := " << Traits<Memory>::PageSize << endl;
-    cout << "RAM_START := 0x" << hex << Traits<MemoryMap>::RamStart << endl;
-    cout << "RAM_END := 0x" << hex << Traits<MemoryMap>::RamEnd << endl;
-    cout << "KERNEL_ADDR := 0x" << hex << Traits<MemoryMap>::KernelAddr << endl;
-    cout << "APPLICATION_ADDR := 0x" << hex << Traits<Application>::Addr << endl;
+    cout << "\nMACHINE_NAME := " << Traits<Machine>::NAME;
+    cout << "\nARCH := " << Traits<CPU>::Architecture;
+    cout << "\nCPUS := " << Traits<CPU>::Count;
+    cout << "\nMEMORY_SIZE := " << Traits<Memory>::Size;
+    cout << "\nPAGE_SIZE := " << Traits<Memory>::PageSize;
+    cout << "\nRAM_START := 0x" << hex << Traits<MemoryMap>::RamStart;
+    cout << "\nRAM_END := 0x" << hex << Traits<MemoryMap>::RamEnd;
+    cout << "\nKERNEL_ADDR := 0x" << hex << Traits<MemoryMap>::KernelAddr;
+    cout << "\nAPPLICATION_ADDR := 0x" << hex << Traits<Application>::Addr;
 }
