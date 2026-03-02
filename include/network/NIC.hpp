9db48a58c82eb0f0c9f582cf1ef746eabc49142a
Ilton Pfleger Júnior <ilton.pfleger@grad.ufsc.br>
2026-03-01 23:01:35 -0300

    remove inheritance from dma in dwmac

--- include/network/NIC.hpp
+++ include/network/NIC.hpp
@@ -8,7 +8,12 @@
 namespace DEPOS {
 
 template <typename Derived> class NIC : public Observed<const unsigned char *, size_t> {
+  public:
+    using Observer = DEPOS::Observer<const unsigned char *, size_t>;
+    using Observed = DEPOS::Observed<const unsigned char *, size_t>;
+    using Device   = Derived;
 
+  private:
     NIC() {
         m_device = Derived::instance();
         new Thread(worker, this);
@@ -20,49 +25,17 @@ template <typename Derived> class NIC : public Observed<const unsigned char *, s
             auto *buffer = self->m_device->receive();
             if (buffer) {
                 self->notify(buffer->data(), buffer->length());
-                self->m_device->free(buffer);
+                self->m_device->release(buffer);
             }
         }
         return 0;
     }
 
   public:
-    // class Buffer {
-    //   public:
-    //     Buffer()
-    //         : m_offset(0),
-    //           m_length(0) {}
-
-    //    void push(unsigned int size) {
-    //        m_offset -= size;
-    //        m_length += size;
-    //    };
-    //    void pop(unsigned int size) {
-    //        m_offset += size;
-    //        m_length -= size;
-    //    };
-
-    //    size_t length() const { return m_length; }
-
-    //    unsigned char *data() { return m_data + m_offset; };
-
-    //  private:
-    //    unsigned char m_data[2048];
-    //    unsigned int m_offset;
-    //    unsigned int m_length;
-    //};
-
-    using Observer = DEPOS::Observer<const unsigned char *, size_t>;
-    using Observed = DEPOS::Observed<const unsigned char *, size_t>;
-    using Device   = Derived;
-
     static void init() { s_instance = new NIC(); }
-
     static auto *instance() { return s_instance; }
-
     int send(const void *b, size_t s) { return m_device->send(b, s); };
-
-    auto address() { return Derived::instance()->address(); }
+    auto address() { return m_device->address(); }
 
   private:
     static inline NIC *s_instance;
