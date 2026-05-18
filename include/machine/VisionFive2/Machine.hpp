11d8f42d049ef33e892c5fd73a4ae6abde560239
Ilton Pfleger Júnioe <pfleger@lisha.ufsc.br>
2026-05-18 14:47:15 -0300

    sync

--- include/machine/VisionFive2/Machine.hpp
+++ include/machine/VisionFive2/Machine.hpp
@@ -2,7 +2,7 @@
 
 #include <Traits.hpp>
 #include <architecture/riscv64/init.hpp>
-#include <machine/VisionFive2/ClockManager.hpp>
+#include <machine/VisionFive2/ClockController.hpp>
 #include <machine/VisionFive2/GPIO.hpp>
 
 namespace DEPOS {
@@ -11,41 +11,32 @@ class VisionFive2 : Driver {
   public:
     static void init() {
         riscv64::init();
-
         if (riscv64::CPU::id() == Traits<CPU>::BSP) {
             /* ---***--- GMAC0 ---***--- */
-            ClockManager::enable(ClockManager::SYSCRG_CLK_GMAC_PHY);
-            ClockManager::enable(ClockManager::SYSCRG_CLK_GMAC0_GTX);
-            ClockManager::enable(ClockManager::SYSCRG_CLK_GMAC_SOURCE);
-            ClockManager::enable(ClockManager::SYSCRG_CLK_GMAC5_AXI64_AHB);
-            ClockManager::enable(ClockManager::SYSCRG_CLK_GMAC5_AXI64_AXI);
-
-            ClockManager::reset(ClockManager::SYSCRG_CLK_RSTN_U1_GMAC5_AXI64_ARESETN_I);
-            ClockManager::reset(ClockManager::SYSCRG_CLK_RSTN_U1_GMAC5_AXI64_HRESET_N);
-
-            ClockManager::divide(ClockManager::AONCRG_CLK_GMAC0_RMII_RTX, 30);
-
-            ClockManager::enable(ClockManager::AONCRG_CLK_GMAC0_TX);
-
-            ClockManager::invert(ClockManager::AONCRG_CLK_GMAC5_AXI64_TX_INVERTER, true);
-
-            ClockManager::multiplex(ClockManager::AONCRG_CLK_GMAC0_TX, 1);
-
-            ClockManager::enable(ClockManager::AONCRG_CLK_GMAC0_AHB);
-            ClockManager::enable(ClockManager::AONCRG_CLK_GMAC0_AXI);
-
-            ClockManager::reset(ClockManager::AONCRG_CLK_RSTN_GMAC5_AXI64_AXI);
-            ClockManager::reset(ClockManager::AONCRG_CLK_RSTN_GMAC5_AXI64_AHB);
+            ClockController::enable(ClockController::SYSCRG_CLK_GMAC_PHY);
+            ClockController::enable(ClockController::SYSCRG_CLK_GMAC0_GTX);
+            ClockController::enable(ClockController::SYSCRG_CLK_GMAC_SOURCE);
+            ClockController::enable(ClockController::SYSCRG_CLK_GMAC5_AXI64_AHB);
+            ClockController::enable(ClockController::SYSCRG_CLK_GMAC5_AXI64_AXI);
+            ClockController::reset(ClockController::SYSCRG_CLK_RSTN_U1_GMAC5_AXI64_ARESETN_I);
+            ClockController::reset(ClockController::SYSCRG_CLK_RSTN_U1_GMAC5_AXI64_HRESET_N);
+            ClockController::divide(ClockController::AONCRG_CLK_GMAC0_RMII_RTX, 30);
+            ClockController::enable(ClockController::AONCRG_CLK_GMAC0_TX);
+            ClockController::invert(ClockController::AONCRG_CLK_GMAC5_AXI64_TX_INVERTER, true);
+            ClockController::multiplex(ClockController::AONCRG_CLK_GMAC0_TX, 1);
+            ClockController::enable(ClockController::AONCRG_CLK_GMAC0_AHB);
+            ClockController::enable(ClockController::AONCRG_CLK_GMAC0_AXI);
+            ClockController::reset(ClockController::AONCRG_CLK_RSTN_GMAC5_AXI64_AXI);
+            ClockController::reset(ClockController::AONCRG_CLK_RSTN_GMAC5_AXI64_AHB);
 
             /* ---***--- CAN0 ---***--- */
-            ClockManager::divide(ClockManager::SYSCRG_CLK_CAN0_CTRL_CORE, 15);
-            ClockManager::enable(ClockManager::SYSCRG_CLK_CAN0_CTRL_APB);
-            ClockManager::enable(ClockManager::SYSCRG_CLK_CAN0_CTRL_TIMER);
-            ClockManager::enable(ClockManager::SYSCRG_CLK_CAN0_CTRL_CORE);
-
-            ClockManager::reset(ClockManager::SYSCRG_CLK_RSTN_U0_CAN_CTRL_APB);
-            ClockManager::reset(ClockManager::SYSCRG_CLK_RSTN_U0_CAN_CTRL_CORE);
-            ClockManager::reset(ClockManager::SYSCRG_CLK_RSTN_U0_CAN_CTRL_TIMER);
+            ClockController::divide(ClockController::SYSCRG_CLK_CAN0_CTRL_CORE, 15);
+            ClockController::enable(ClockController::SYSCRG_CLK_CAN0_CTRL_APB);
+            ClockController::enable(ClockController::SYSCRG_CLK_CAN0_CTRL_TIMER);
+            ClockController::enable(ClockController::SYSCRG_CLK_CAN0_CTRL_CORE);
+            ClockController::reset(ClockController::SYSCRG_CLK_RSTN_U0_CAN_CTRL_APB);
+            ClockController::reset(ClockController::SYSCRG_CLK_RSTN_U0_CAN_CTRL_CORE);
+            ClockController::reset(ClockController::SYSCRG_CLK_RSTN_U0_CAN_CTRL_TIMER);
             GPIO::map(GPIO::OutputSignal::GPO_SYS_IOMUX_U0_CAN_CTRL_TXD, 42);
             GPIO::map(GPIO::InputSignal::GPI_SYS_IOMUX_U0_CAN_CTRL_RXD, 43);
             GPIO::map(GPIO::OutputSignal::GPO_SYS_IOMUX_U0_CAN_CTRL_STB, 47);
